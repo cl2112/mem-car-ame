f3fdf9b73f02234f517df8ba795653233a446e18
cl2112 <23705622+cl2112@users.noreply.github.com>
2018-09-08 08:39:33 -0400

    Added the evidence to the cardFronts in the Game.render function.

--- js/DaGame.js
+++ js/DaGame.js
@@ -19,10 +19,95 @@ const Game = {
             numberOfPairs: 4,
             correctMatches: 0,
             cardFronts: [
-                "angel-1.png",
-                "angel-2.png",
-                "angel-3.png",
-                "angel-4.png"
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-0-2-01-00.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-0-2-01-02.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-0-2-01-04.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-0-2-01-06.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-0-2-03-00.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-0-2-03-02.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-0-2-03-04.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-0-2-03-06.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-0-2-05-00.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-0-2-05-02.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-0-2-05-04.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-0-2-07-00.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-0-2-07-02.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-0-2-07-04.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-0-2-09-00.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-0-2-09-02.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-0-2-09-04.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-0-2-11-00.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-0-2-11-02.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-0-2-11-04.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-0-2-13-00.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-0-2-13-02.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-0-2-13-04.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-00-02.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-00-04.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-00-08.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-00-10.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-00-12.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-00-14.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-00-16g.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-00-18.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-02-02.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-02-04.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-02-06.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-02-08.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-02-10.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-02-12.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-02-14.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-02-16.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-02-18.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-04-02.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-04-04.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-04-06.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-04-08.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-04-10.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-04-12.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-04-14.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-04-16.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-04-18.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-06-02.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-06-04.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-06-06.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-06-08.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-06-10.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-06-12.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-06-14.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-06-16.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-06-18.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-08-00.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-08-02.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-08-04.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-08-06.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-08-08.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-08-10.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-08-12.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-08-14.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-08-16.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-08-18.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-10-00.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-10-02.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-10-04.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-10-06.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-10-08.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-10-10.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-10-12.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-10-14.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-10-16.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-10-18.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-12-00.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-12-02.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-12-04.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-12-06.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-12-08.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-12-10.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-12-12.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-12-14.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-12-16.png",
+                "DS DSi - Phoenix Wright Ace Attorney - Evidence-1-1-12-18.png",
+                "evidence-Attorneys-Badge.png"
             ],
             cardFlipped: "",
             cardsObjects: [],
@@ -71,12 +156,12 @@ const Game = {
     render: function() {
         const cards = this.createCards();
 
-        cards.forEach((v,i) => {
+        cards.forEach((v, i) => {
             const cardBack = document.createElement("div");
             const cardFront = document.createElement("div");
             const card = document.createElement("div");
 
-            cardFront.style.backgroundImage = `url(./images/cards/${v})`;
+            cardFront.style.backgroundImage = `url("./images/card-images/${v}")`;
             cardFront.classList.add("card-front");
 
             card.classList.add("card");
@@ -90,10 +175,12 @@ const Game = {
 
             document.querySelector(".wrapper").appendChild(card);
 
-            document.querySelectorAll(".card")[i].addEventListener("click", function() {
-                console.log(this)
-                this.classList.toggle("card-flipped");
-            });
+            document
+                .querySelectorAll(".card")
+                [i].addEventListener("click", function() {
+                    console.log(this);
+                    this.classList.toggle("card-flipped");
+                });
         });
 
         // document.querySelector(".card").addEventListener("click", function() {
